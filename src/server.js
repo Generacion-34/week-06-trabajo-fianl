4f31f60799cf1409c6aaeea4b85ddb34bbaef10e
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-04-22 23:13:11 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const app = require('./app');
+const sequelize = require('./utils/connection');
+
+const PORT = process.env.PORT || 8080;
+
+const main = async () => {
+    try {
+        sequelize.sync();
+        console.log("DB connected");
+        app.listen(PORT);
+        console.log(`👉 Server running on port ${PORT}`);
+        console.log(`👉 Link http://localhost:${PORT}`);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main();
