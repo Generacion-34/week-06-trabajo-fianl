7634bbb5ff5d7206bcf58b7078c691a55f4874db
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-04-24 23:33:08 -0300

    advances

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -21,7 +21,6 @@ beforeAll(async () => {
   TOKEN = res.body.token
 })
 
-
 test("GET -> BASE_URL, should return statusCode 200, and res.body.length === 1", async () => {
   const res = await request(app)
     .get(BASE_URL)
@@ -45,10 +44,66 @@ test("POST -> BASE_URL, should return statusCode 201, and res.body.firstName ===
     .post(BASE_URL)
     .send(user)
 
-
   userId = res.body.id
 
   expect(res.statusCode).toBe(201)
   expect(res.body).toBeDefined()
   expect(res.body.firstName).toBe(user.firstName)
+})
+
+
+//testeo de rutas dinamicas
+
+test("PUT -> 'BASE_URL/:id', should return status code 200, res.body.lastName === userUpdate.lastName", async () => {
+  const userUpdate = {
+    lastName: "Andrade"
+  }
+
+  const res = await request(app)
+    .put(`${BASE_URL}/${userId}`)
+    .send(userUpdate)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.lastName).toBe(userUpdate.lastName)
+})
+
+test("POST -> 'BASE_URL/login', sould return statusCode 200, res.body.user.email === user.email and res.body.token to be defined ", async () => {
+
+  const user = {
+    email: "maicol@gmail.com",
+    password: "maicol1234"
+  }
+
+  const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(user)
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.user.email).toBe(user.email)
+  expect(res.body.token).toBeDefined()
+})
+
+test("POST 'BASE_URL/login', should return statusCode 401", async () => {
+  const userInvalid = {
+    email: "maicol@gmail.com",
+    password: "Invalid password"
+  }
+
+  const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(userInvalid)
+
+  expect(res.statusCode).toBe(401)
+})
+
+
+test("Delete ->'BASE_URL/:id', should return statusCode 204", async () => {
+  const res = await request(app)
+    .delete(`${BASE_URL}/${userId}`)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.statusCode).toBe(204)
 })
