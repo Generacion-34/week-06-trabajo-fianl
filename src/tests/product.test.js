732b1ccaa7fb8914e6a0ef92b7ae58245d499b0d
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-04-26 00:18:59 -0300

    advances

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -69,5 +69,30 @@ test('GET -> BASE_URL/:id, should return statusCode 201, and res.body.length ===
   expect(res.body).toBeDefined()
   expect(res.body.title).toBe(product.title)
 
+})
+
+test("PUT -> BASE_URL, should return statusCode 200, and res.body.title === bodyUpdate.title", async () => {
+  const bodyUpdate = {
+    title: "iphone 15 pro max"
+  }
+
+  const res = await request(app)
+    .put(`${BASE_URL}/${productId}`)
+    .send(bodyUpdate)
+    .set("Authorization", `Bearer ${TOKEN}`)
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.title).toBe(bodyUpdate.title)
+
+})
+
+test('Delete -> BASE_URL, should return statusCode 204', async () => {
+  const res = await request(app)
+    .delete(`${BASE_URL}/${productId}`)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.statusCode).toBe(204)
+
   await category.destroy() //! siempre va en la ultima linea, del ultimo test del archivo
 })
