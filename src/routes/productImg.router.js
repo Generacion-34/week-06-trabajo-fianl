658fa48a9c298e813b3805db91af40a628a5ea20
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-05-01 01:12:25 -0300

    advances

--- src/routes/productImg.router.js
+++ src/routes/productImg.router.js
@@ -1,11 +1,12 @@
 const { getAll, create, remove, } = require('../controllers/productImg.controllers');
 const express = require('express');
+const upload = require('../utils/multer');
 
 const routerProductImg = express.Router();
 
 routerProductImg.route('/')
   .get(getAll)
-  .post(create);
+  .post(upload.single('image'), create);
 
 routerProductImg.route('/:id')
   .delete(remove)
